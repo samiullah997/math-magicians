d39f4e2c52a99269f8dd1189fb27c8659dc6732e
samiullah997 <samiullahk997@gmail.com>
2022-09-13 19:18:01 +0500

    solve file not found error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Calculator from './components/Calculator';
 import Home from './components/Home';
+import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 
 class App extends Component {
